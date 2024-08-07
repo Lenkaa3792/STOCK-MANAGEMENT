5a2badacfd10f5dc83e92b955a3de6f9f640186f
Kariuki Karanja <kariukimarkbrian@gmail.com>
2024-08-07 15:33:18 +0300

    :bug: fix signup and sign in routes and logic

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,19 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def create
+    self.resource = User.find_by(email: params[:user][:email])
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+    if resource && resource.valid_password?(params[:user][:password])
+      sign_in(resource_name, resource)
+      session[:user_id] = resource.id
+      session_id = SecureRandom.uuid
+      render json: { success: true, message: 'Logged in successfully.', session_id: session_id, resource: resource }
+    elsif !resource
+      render json: { success: false, message: 'Please enter a valid email address' }, status: :not_found
+    elsif !resource.valid_password?(params[:user][:password])
+      render json: { success: false, message: 'Invalid password. Please enter a valid password' }, status: :unauthorized
+    else
+      render json: { success: false }, status: :unauthorized
+    end
+  end
 end
